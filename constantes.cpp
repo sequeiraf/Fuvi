a95d7162dd0ec41fe3261c8757cad19a5232cc1e
Natalia Sequeira <electronica2022@fuvi.net>
2022-10-04 10:14:54 -0600

    Modifico constantes.cpp y agrego Pitagoras.cpp

---
+++ constantes.cpp
@@ -0,0 +1,21 @@
+/*
+Nombre del archivo: Plantilla.cpp
+Autor:Natalia Sequeira
+Lugar: Instituto Tecnológico Victoria
+Instrucciones: Uso de las constantes
+*/
+
+#include<iostream>
+
+using namespace std;
+//Variables globales
+const double pi = 3.141592;
+
+int main(){
+	double radio, area;
+	cout << "Ingrese el radio del circulo: ";
+	cin >> radio;
+	double = pi * pow(radio,2);
+	cout << "El área de un círculo con radio" <<radio << "es" << area;
+	retur 0;
+}
